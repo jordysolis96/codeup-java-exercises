008c1d06efa753d1cf2a1a4bda014ccec063a2d6
jordy muniz <jordy.muniz96@gmail.com>
2020-12-04 12:31:34 -0600

    stings exercise

---
+++ src/Bob.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[]args){
+//        Scanner userInput = new Scanner(System.in);
+//        String question = "Sure";
+//        String Yell = "Whoa, chill out!";
+//        String coldShoulder = "Fine. Be that way!";
+//        String anyElse = "Whatever.";
+//        System.out.println("Would you like to talk to bob? [y/n]");
+//        String wantsConversation = userInput.next();
+////        String empty = userInput.nextLine();
+//        if(wantsConversation.contains("y")) {
+//            userInput.nextLine();
+//            if (userInput.equals("?")) {
+//                System.out.println(question);
+//            } else if (userInput.equals("!")) {
+//                System.out.println(Yell);
+//            } else if (anyElse.isEmpty()) {
+//                System.out.println(coldShoulder);
+//            } else {
+//                System.out.println(anyElse);
+//            }
+//        }
+
+
+        Scanner scanner = new Scanner(System.in);
+        String userResponse;
+
+        do {
+
+            System.out.print("What do you want to say to Bob? ");
+
+            String input = scanner.nextLine();
+            String response;
+
+            if (input.endsWith("?")) {
+                response = "Sure.";
+            } else if (input.endsWith("!")) {
+                response = "Whoa, chill out!";
+            } else if (input.isEmpty()) {
+                response = "Fine. Be that way!";
+            } else {
+                response = "Whatever.";
+            }
+
+            System.out.printf("Bob says: \"%s\"\n\n", response);
+
+            System.out.print("Did you have anything else to say to Bob? [y/n] ");
+
+            userResponse = scanner.nextLine();
+
+        } while (userResponse.equalsIgnoreCase("y") || userResponse.equalsIgnoreCase("yes"));
+
+    }
+}
