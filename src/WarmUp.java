e0c54300f73afd899438fd91069470bd2c2b526b
jordy muniz <jordy.muniz96@gmail.com>
2020-12-04 09:41:26 -0600

    String Lecture

---
+++ src/WarmUp.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class WarmUp {
+
+    // TODO: The following app is broken and needs to be debugged. Change the code below to run in the expected manner.
+    //  In this exercise, assume the user will always enter a valid data type (an integer, then a string).
+    //  When working correctly, the program should have a similar output to the following:
+    /*
+        Welcome to 'PRINT THE VEGETABLE COUNT' app!
+
+        How many vegetables do you have? 3
+        What kind of vegetable? (use a singular noun, e.g. 'carrot' vs. 'carrots'): carrot
+        You have 3 carrots!
+
+
+        Process finished with exit code 0
+     */
+
+    // HINT: You can fix the unexpected behavior by altering or adding a single line
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to 'PRINT THE VEGETABLE COUNT' app!\n");
+        System.out.print("How many vegetables do you have? ");
+        int count = sc.nextInt();
+        sc.nextLine();//added a next line so that the final message won't display and not take in the nextLine coming up as a valid input
+        System.out.print("What kind of vegetable? (use a singular noun, e.g. 'carrot' vs. 'carrots'): ");
+        String veggie = sc.nextLine();
+        System.out.printf("You have %d %ss!\n\n", count, veggie);
+    }
+}
