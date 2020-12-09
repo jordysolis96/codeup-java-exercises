48b3d58c7a9e5a16b855d60e4cffd4ba443068c0
jordy muniz <jordy.muniz96@gmail.com>
2020-12-09 10:20:32 -0600

    shapes package

---
+++ src/shapes/Circle.java
@@ -0,0 +1,28 @@
+package shapes;
+
+public class Circle {
+
+    private double radius;
+    private static int totalCircles = 0;
+
+    public Circle(double radius) {
+        this.radius = radius;
+        Circle.totalCircles++;
+    }
+
+    public static void totalCirclesCreated() {
+        System.out.printf("Total circles created: %d\n", Circle.totalCircles);
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public void printInfo() {
+        System.out.printf("Area: %f\nCircumference: %f\n", this.getArea(), this.getCircumference());
+    }
+
+}
---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,27 @@
+package shapes;
+
+import util.Input;
+
+public class CircleApp {
+
+
+    public static void main(String[] args) {
+
+        boolean repeat;
+
+        do {
+            Input input = new Input();
+
+            Circle c = new Circle(input.getDouble("Enter the radius:"));
+
+            c.printInfo();
+
+            repeat = input.yesNo("Do you want to enter another radius?");
+
+        } while (repeat);
+
+        Circle.totalCirclesCreated();
+
+    }
+
+}
