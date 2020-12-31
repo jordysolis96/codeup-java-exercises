a5b529508c6ba1b49b0ac0dccfac21f64da018bc
jordy muniz <jordy.muniz96@gmail.com>
2020-12-30 22:16:54 -0600

    String exercise redo

--- src/Bob.java
+++ src/Bob.java
@@ -27,30 +27,30 @@ public class Bob {
         Scanner scanner = new Scanner(System.in);
         String userResponse;
 
-        do {
-
-            System.out.print("What do you want to say to Bob? ");
-
-            String input = scanner.nextLine();
-            String response;
-
-            if (input.endsWith("?")) {
-                response = "Sure.";
-            } else if (input.endsWith("!")) {
-                response = "Whoa, chill out!";
-            } else if (input.isEmpty()) {
-                response = "Fine. Be that way!";
-            } else {
-                response = "Whatever.";
-            }
-
-            System.out.printf("Bob says: \"%s\"\n\n", response);
-
-            System.out.print("Did you have anything else to say to Bob? [y/n] ");
-
-            userResponse = scanner.nextLine();
-
-        } while (userResponse.equalsIgnoreCase("y") || userResponse.equalsIgnoreCase("yes"));
+//        do {
+//
+//            System.out.print("What do you want to say to Bob? ");
+//
+//            String input = scanner.nextLine();
+//            String response;
+//
+//            if (input.endsWith("?")) {
+//                response = "Sure.";
+//            } else if (input.endsWith("!")) {
+//                response = "Whoa, chill out!";
+//            } else if (input.isEmpty()) {
+//                response = "Fine. Be that way!";
+//            } else {
+//                response = "Whatever.";
+//            }
+//
+//            System.out.printf("Bob says: \"%s\"\n\n", response);
+//
+//            System.out.print("Did you have anything else to say to Bob? [y/n] ");
+//
+//            userResponse = scanner.nextLine();
+//
+//        } while (userResponse.equalsIgnoreCase("y") || userResponse.equalsIgnoreCase("yes"));
 
     }
 }
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,21 +1,33 @@
 public class StringExercise {
     public static void main(String[]arg){
-        String output = "We don't need no education\nWe don't need no thought control";
+//        String output = "We don't need no education\nWe don't need no thought control";
+//
+//        System.out.println(output);
+//
+//        output = "Check \"this\" out!, \"s inside of \"s!";
+//
+//        System.out.println(output);
+//
+//        output = "In windows, the main drive is usually C:\\";
+//
+//        System.out.println(output);
+//
+//        output = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslashes \\\\\\!";
+//
+//        System.out.print(output);
 
-        System.out.println(output);
+        //This is a redo of the exercise for extra practice
+        String newOutput = "We don't need no education\nWe don't need no thought control";
+        System.out.println(newOutput);
 
-        output = "Check \"this\" out!, \"s inside of \"s!";
+        newOutput = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(newOutput);
 
-        System.out.println(output);
+        newOutput = "In windows, the main  drive is usually c:\\";
+        System.out.println(newOutput);
 
-        output = "In windows, the main drive is usually C:\\";
-
-        System.out.println(output);
-
-        output = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslashes \\\\\\!";
-
-        System.out.print(output);
-
+        newOutput = "I can do backslashes \\, double backslashes \\\\,%n and the amazing triple backslash\\\\\\!";
+        System.out.println(newOutput);
 
     }
 }
