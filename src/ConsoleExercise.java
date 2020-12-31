83f7f24a8dba9c2782899f9934f0aa3541b996dc
jordy muniz <jordy.muniz96@gmail.com>
2020-12-30 18:54:59 -0600

    redo of the consoleIO exercise

--- src/ConsoleExercise.java
+++ src/ConsoleExercise.java
@@ -36,15 +36,49 @@ public class ConsoleExercise {
 //        //.next only return the one token or string word at a time
 
 
-        System.out.print("\nThis program wil take in length, width and height, to calculate perimeter, area and volume\n");
-        System.out.print("Length:\n");
-        String lengthInput = userInput.nextLine();
-        System.out.print("Width:\n");
-        String widthInput = userInput.nextLine();
+//        System.out.print("\nThis program wil take in length, width and height, to calculate perimeter, area and volume\n");
+//        System.out.print("Length:\n");
+//        String lengthInput = userInput.nextLine();
+//        System.out.print("Width:\n");
+//        String widthInput = userInput.nextLine();
+//        System.out.print("Height:\n");
+//        String heightInput = userInput.nextLine();
+//        float length = parseFloat(lengthInput);
+//        float width = parseFloat(widthInput);
+//        float height = parseFloat(heightInput);
+//        float area = length * width;
+//        float perimeter = (2 * length) + (2 * width);
+//        float volume = length * width * height;
+//        System.out.format("Perimeter: %.2f. Area: %.2f. Volume: %.2f", perimeter, area, volume);
+//
+//        userInput.useDelimiter("\n");
+
+        // This is a redo of the exercise for extra practice
+        System.out.println("Enter a integer!");
+        int integer = userInput.nextInt();
+        System.out.printf("The integer you entered is %s%n", integer);
+
+        System.out.println("please enter meal, drink, and a side");
+        String meal = userInput.next();
+        String drink = userInput.next();
+        String side = userInput.next();
+        System.out.printf("Meal: %s%n", meal);
+        System.out.printf("Drink: %s%n", drink);
+        System.out.printf("Side: %s%n", side);
+
+        System.out.println("Please enter a sentence!");
+        String userSentence = userInput.nextLine();
+        System.out.printf("Sentence: %s%n", userSentence);
+
+        System.out.println("Enter the length and width of the classroom!");
+        System.out.println("Length: ");
+        String userlength = userInput.nextLine();
+        System.out.println("Width: ");
+        String userwidth = userInput.nextLine();
         System.out.print("Height:\n");
         String heightInput = userInput.nextLine();
-        float length = parseFloat(lengthInput);
-        float width = parseFloat(widthInput);
+        float length = parseFloat(userlength);
+        float width = parseFloat(userwidth);
         float height = parseFloat(heightInput);
         float area = length * width;
         float perimeter = (2 * length) + (2 * width);
