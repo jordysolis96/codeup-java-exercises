e1d6a468f0e33e3846eabc81c6676a3e04969387
jordy muniz <jordy.muniz96@gmail.com>
2021-01-05 16:25:04 -0600

    refactor bob exercise

--- src/Bob.java
+++ src/Bob.java
@@ -3,27 +3,27 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[]args){
         Scanner userInput = new Scanner(System.in);
-        String question = "Sure";
-        String Yell = "Whoa, chill out!";
-        String coldShoulder = "Fine. Be that way!";
-        String anyElse = "Whatever.";
-        System.out.println("Would you like to talk to bob? [y/n]");
-        String wantsConversation = userInput.next();
-        String empty = userInput.nextLine();
-        if(wantsConversation.contains("y")) {
-            userInput.nextLine();
-            if (userInput.equals("?")) {
-                System.out.println(question);
-            } else if (userInput.equals("!")) {
-                System.out.println(Yell);
-            } else if (anyElse.isEmpty()) {
-                System.out.println(coldShoulder);
-            } else {
-                System.out.println(anyElse);
-            }
-        }
+//        String question = "Sure";
+//        String Yell = "Whoa, chill out!";
+//        String coldShoulder = "Fine. Be that way!";
+//        String anyElse = "Whatever.";
+//        System.out.println("Would you like to talk to bob? [y/n]");
+//        String wantsConversation = userInput.next();
+//        String empty = userInput.nextLine();
+//        if(wantsConversation.contains("y")) {
+//            userInput.nextLine();
+//            if (userInput.equals("?")) {
+//                System.out.println(question);
+//            } else if (userInput.equals("!")) {
+//                System.out.println(Yell);
+//            } else if (anyElse.isEmpty()) {
+//                System.out.println(coldShoulder);
+//            } else {
+//                System.out.println(anyElse);
+//            }
+//        }
 
-
+//--------------------Second redo--------------------------
 //        Scanner scanner = new Scanner(System.in);
 //        String userResponse;
 
@@ -52,5 +52,26 @@ public class Bob {
 //
 //        } while (userResponse.equalsIgnoreCase("y") || userResponse.equalsIgnoreCase("yes"));
 
+//--------------------------------Third redo-------------------------------------------------------------
+        String question = "Sure";
+        String Yell = "Whoa, chill out!";
+        String coldShoulder = "Fine. Be that way!";
+        String anyElse = "Whatever.";
+        System.out.println("Would you like to talk to bob? [y/n]");
+        String wantsConversation = userInput.next();
+        String empty = userInput.nextLine();
+        if(wantsConversation.contains("y")) {
+            System.out.println("Say something to bob!");
+            String Input = userInput.nextLine();
+            if (Input.endsWith("?")) {
+                System.out.println(question);
+            } else if (Input.endsWith("!")) {
+                System.out.println(Yell);
+            } else if (Input.isEmpty()) {
+                System.out.println(coldShoulder);
+            } else {
+                System.out.println(anyElse);
+            }
+        }
     }
 }
