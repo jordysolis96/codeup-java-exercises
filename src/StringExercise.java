f9dfc235c5de265a5be1581b57f323ac9c155dd2
jordy muniz <jordy.muniz96@gmail.com>
2021-01-05 15:56:54 -0600

    string exercise third redo

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -17,17 +17,30 @@ public class StringExercise {
 //        System.out.print(output);
 
         //This is a redo of the exercise for extra practice
-        String newOutput = "We don't need no education\nWe don't need no thought control";
-        System.out.println(newOutput);
+//        String newOutput = "We don't need no education\nWe don't need no thought control";
+//        System.out.println(newOutput);
+//
+//        newOutput = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(newOutput);
+//
+//        newOutput = "In windows, the main  drive is usually c:\\";
+//        System.out.println(newOutput);
+//
+//        newOutput = "I can do backslashes \\, double backslashes \\\\,%n and the amazing triple backslash\\\\\\!";
+//        System.out.println(newOutput);
+//
 
-        newOutput = "Check \"this\" out!, \"s inside of \"s!";
-        System.out.println(newOutput);
+//------------------------Third redo------------------------
+        String one = "We dont need no eductaion\nWe don't need no thought control";
+        System.out.println(one);
 
-        newOutput = "In windows, the main  drive is usually c:\\";
-        System.out.println(newOutput);
+        String two = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(two);
 
-        newOutput = "I can do backslashes \\, double backslashes \\\\,%n and the amazing triple backslash\\\\\\!";
-        System.out.println(newOutput);
+        String three = "In windows, the main drive is usually C:\\";
+        System.out.println(three);
 
+        String four = "I can do backslashes \\, double backslashes\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(four);
     }
 }
