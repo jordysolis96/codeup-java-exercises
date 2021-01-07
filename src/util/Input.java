fff27a24c2566af2d0ad5eded34a3c06f0799765
jordy muniz <jordy.muniz96@gmail.com>
2021-01-07 00:06:30 -0600

    input java redo checkpoint

--- src/util/Input.java
+++ src/util/Input.java
@@ -2,144 +2,157 @@ package util;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+//public class Input {
+//
+//    private Scanner scanner;
+//
+//    public Input() {
+//        this.scanner = new Scanner(System.in);
+//    }
+//
+//    public String getString() {
+//        return this.scanner.nextLine();
+//    }
+//
+//    public boolean yesNo() {
+//        System.out.print("Would you like to run again? [y/n] ");
+//
+//        String input = this.getString();
+//
+//        return (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
+//    }
+//
+//    public boolean yesNo(String prompt) {
+//        System.out.print(prompt + " [y/n] ");
+//
+//        String input = this.getString();
+//
+//        return (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
+//    }
+//
+//    public int getInt() {
+//        int value;
+//
+//        do {
+//            value = getInt("Enter an integer:");
+//        } while (value < 0);
+//
+//        return value;
+//    }
+//
+//    public int getInt(String prompt) {
+//        int value;
+//        do {
+//            System.out.print(prompt + " ");
+//            String userInput = this.getString();
+//
+//            try {
+//                value = Integer.parseInt(userInput);
+//            } catch (Exception e) {
+//                System.out.println("Invalid Input: " + userInput);
+//                value =  -1;
+//            }
+//
+//        } while (value < 0);
+//
+//        return value;
+//    }
+//
+//    public int getInt(int min, int max) {
+//
+//        int userInput;
+//
+//        do {
+//            userInput = this.getInt(String.format("Enter an integer between %d and %d:", min, max));
+//
+//            if (userInput < min || userInput > max) {
+//                System.out.printf("Invalid input: %d\n", userInput);
+//            } else {
+//                break;
+//            }
+//
+//        } while (true);
+//
+//        return userInput;
+//    }
+//
+//    public int getInt(int min, int max, String prompt) {
+//
+//        int userInput;
+//
+//        do {
+//            userInput = this.getInt(prompt);
+//
+//            if (userInput < min || userInput > max) {
+//                System.out.printf("Invalid input: %d\n", userInput);
+//            } else {
+//                break;
+//            }
+//
+//        } while (true);
+//
+//        return userInput;
+//    }
+//
+//    public double getDouble() {
+//
+//        double value;
+//
+//        do {
+//            value = getDouble("Enter a number");
+//        } while (value < 0);
+//
+//        return value;
+//    }
+//
+//    public double getDouble(String prompt) {
+//        double value;
+//        do {
+//            System.out.print(prompt + " ");
+//            String userInput = this.getString();
+//
+//
+//            try {
+//                value = Double.parseDouble(userInput);
+//            } catch (Exception e) {
+//                System.out.println("Invalid Input: " + userInput);
+//                value =  -1;
+//            }
+//
+//        } while (value < 0);
+//
+//        return value;
+//    }
+//
+//    public double getDouble(double min, double max) {
+//
+//        double userInput;
+//
+//        do {
+//            userInput = this.getDouble(String.format("Enter a number between %f and %f:", min, max));
+//
+//            if (userInput < min || userInput > max) {
+//                System.out.printf("Invalid input: %f\n", userInput);
+//            } else {
+//                break;
+//            }
+//
+//        } while (true);
+//
+//        return userInput;
+//    }
+//
+//}
+//---------------------------------redo for practice---------------------------------------------------------
 public class Input {
-
     private Scanner scanner;
 
-    public Input() {
-        this.scanner = new Scanner(System.in);
-    }
-
-    public String getString() {
+    public String getString(){
         return this.scanner.nextLine();
     }
 
-    public boolean yesNo() {
-        System.out.print("Would you like to run again? [y/n] ");
-
-        String input = this.getString();
-
-        return (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
+    public boolean yesNo(){
+        System.out.println("enter [y/n]");
+        return this.scanner.nextBoolean();
     }
-
-    public boolean yesNo(String prompt) {
-        System.out.print(prompt + " [y/n] ");
-
-        String input = this.getString();
-
-        return (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"));
-    }
-
-    public int getInt() {
-        int value;
-
-        do {
-            value = getInt("Enter an integer:");
-        } while (value < 0);
-
-        return value;
-    }
-
-    public int getInt(String prompt) {
-        int value;
-        do {
-            System.out.print(prompt + " ");
-            String userInput = this.getString();
-
-            try {
-                value = Integer.parseInt(userInput);
-            } catch (Exception e) {
-                System.out.println("Invalid Input: " + userInput);
-                value =  -1;
-            }
-
-        } while (value < 0);
-
-        return value;
-    }
-
-    public int getInt(int min, int max) {
-
-        int userInput;
-
-        do {
-            userInput = this.getInt(String.format("Enter an integer between %d and %d:", min, max));
-
-            if (userInput < min || userInput > max) {
-                System.out.printf("Invalid input: %d\n", userInput);
-            } else {
-                break;
-            }
-
-        } while (true);
-
-        return userInput;
-    }
-
-    public int getInt(int min, int max, String prompt) {
-
-        int userInput;
-
-        do {
-            userInput = this.getInt(prompt);
-
-            if (userInput < min || userInput > max) {
-                System.out.printf("Invalid input: %d\n", userInput);
-            } else {
-                break;
-            }
-
-        } while (true);
-
-        return userInput;
-    }
-
-    public double getDouble() {
-
-        double value;
-
-        do {
-            value = getDouble("Enter a number");
-        } while (value < 0);
-
-        return value;
-    }
-
-    public double getDouble(String prompt) {
-        double value;
-        do {
-            System.out.print(prompt + " ");
-            String userInput = this.getString();
-
-
-            try {
-                value = Double.parseDouble(userInput);
-            } catch (Exception e) {
-                System.out.println("Invalid Input: " + userInput);
-                value =  -1;
-            }
-
-        } while (value < 0);
-
-        return value;
-    }
-
-    public double getDouble(double min, double max) {
-
-        double userInput;
-
-        do {
-            userInput = this.getDouble(String.format("Enter a number between %f and %f:", min, max));
-
-            if (userInput < min || userInput > max) {
-                System.out.printf("Invalid input: %f\n", userInput);
-            } else {
-                break;
-            }
-
-        } while (true);
-
-        return userInput;
-    }
-
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,30 +1,30 @@
 package util;
 
-public class InputTest {
-
-    public static void main(String[] args) {
-
-        boolean repeat;
-
-        do {
-            String userStr;
-            Input test = new Input();
-
-            int userInt = test.getInt();
-            System.out.printf("User Input: %d\n", userInt);
-
-            userInt = test.getInt(1, 10);
-            System.out.printf("User Input: %d\n", userInt);
-
-            double userDouble = test.getDouble();
-            System.out.printf("User Input: %f\n", userDouble);
-
-            userDouble = test.getDouble(1.5, 9.5);
-            System.out.printf("User Input: %f\n", userDouble);
-
-            repeat = test.yesNo("Do you want to run the test again?");
-
-        } while (repeat) ;
-    }
-
-}
+//public class InputTest {
+//
+//    public static void main(String[] args) {
+//
+//        boolean repeat;
+//
+//        do {
+//            String userStr;
+//            Input test = new Input();
+//
+//            int userInt = test.getInt();
+//            System.out.printf("User Input: %d\n", userInt);
+//
+//            userInt = test.getInt(1, 10);
+//            System.out.printf("User Input: %d\n", userInt);
+//
+//            double userDouble = test.getDouble();
+//            System.out.printf("User Input: %f\n", userDouble);
+//
+//            userDouble = test.getDouble(1.5, 9.5);
+//            System.out.printf("User Input: %f\n", userDouble);
+//
+//            repeat = test.yesNo("Do you want to run the test again?");
+//
+//        } while (repeat) ;
+//    }
+//
+//}
